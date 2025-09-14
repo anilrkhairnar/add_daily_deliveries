7bfcf8ce95ab4087723dc7ea3921dcecd089dc65
Anil Khairnar <anilrkhairnar@gmail.com>
2025-09-14 17:48:16 +0530

    testing database query

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+import { Client, TablesDB } from "node-appwrite";
+
+const APPWRITE_ENDPOINT = process.env.APPWRITE_ENDPOINT;
+const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID;
+const APPWRITE_API_KEY = process.env.APPWRITE_API_KEY;
+const APPWRITE_DATABASE_ID = process.env.APPWRITE_DATABASE_ID;
+const APPWRITE_TABLE = process.env.APPWRITE_TABLE;
 export default async ({ req, res }) => {
-  return res.send("Hello World");
+  if (req.method === "GET") {
+    const client = new Client();
+    const tablesDB = new TablesDB(client);
+    client
+      .setEndpoint(APPWRITE_ENDPOINT) // Your API Endpoint
+      .setProject(APPWRITE_PROJECT_ID) // Your project ID
+      .setKey(APPWRITE_API_KEY); // Your secret API key
+
+    let response = await tablesDB.listRows(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_TABLE
+    );
+    return res.send(response.documents);
+  }
 };
