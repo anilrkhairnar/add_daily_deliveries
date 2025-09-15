291f2e5859dcaa756aa0e0164c590cfdcc9fb9da
Anil Khairnar <anilrkhairnar@gmail.com>
2025-09-15 12:21:05 +0530

    added post method to add json data in bulk

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { Client, TablesDB } from "node-appwrite";
+import { Client, Databases } from "node-appwrite";
+
+import deliveries from "./deliveries.json";
 
 const APPWRITE_ENDPOINT = process.env.APPWRITE_ENDPOINT;
 const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID;
@@ -6,20 +8,39 @@ const APPWRITE_API_KEY = process.env.APPWRITE_API_KEY;
 const APPWRITE_DATABASE_ID = process.env.APPWRITE_DATABASE_ID;
 const APPWRITE_TABLE = process.env.APPWRITE_TABLE;
 export default async ({ req, res }) => {
-  if (req.method == "GET") {
-    const client = new Client();
-    const tablesDB = new TablesDB(client);
-    client
-      .setEndpoint(APPWRITE_ENDPOINT) // Your API Endpoint
-      .setProject(APPWRITE_PROJECT_ID) // Your project ID
-      .setKey(APPWRITE_API_KEY); // Your secret API key
+  const client = new Client();
+  const database = new Databases(client);
+  client
+    .setEndpoint(APPWRITE_ENDPOINT) // Your API Endpoint
+    .setProject(APPWRITE_PROJECT_ID) // Your project ID
+    .setKey(APPWRITE_API_KEY); // Your secret API key
 
-    let response = await tablesDB.listRows(
+  console.log("function is working");
+
+  if (req.method == "GET") {
+    let response = await database.listDocuments(
       APPWRITE_DATABASE_ID,
       APPWRITE_TABLE
     );
-    console.log(response);
-    return res.send(response);
+
+    console.log("response is here", response);
+
+    return res.send({
+      status: 200,
+      message: "Data fetched successfully",
+      data: response,
+    });
+  } else if (req.method == "POST") {
+    const delivery = database.createRows(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_TABLE,
+      deliveries
+    );
+
+    return res.send({
+      status: 200,
+      message: "Data added successfully",
+      data: delivery,
+    });
   }
-  console.log("function is working");
 };
